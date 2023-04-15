dd71afc5084a66b5e1c57faa184194c0472c3cbe
Shohei <dreamcampus1@gmail.com>
2023-04-16 08:28:39 +0900

    reacr18から17にダウングレードしたことで不具合発生したのでsrc/index.jsを修正した

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -6,21 +6,40 @@ import { fetchRestaurants } from '../apis/restaurants';
 
 //images
 import MainLogo from '../images/logo.png';
-import MainCoverImage from '../images/main-cover-image.ong';
+import MainCoverImage from '../images/main-cover-image.png';
 
-const HeaderWrapper = styled.div
-display: flex;
-justify - content: flex - start;
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const MainCover = styled.img`
+  height: 600px;
+`;
 
 export const Restaurants = () => {
   useEffect(() => {
-    fetchRestaurants().then((data) =>
-      console.log(data))
+    fetchRestaurants()
+      .then((data) =>
+        console.log(data))
   }, [])
 
   return (
     <Fragment>
-      レストラン一覧
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo" />
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
     </Fragment>
   )
 
